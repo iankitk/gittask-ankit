841d5fddf37d351c805784e36f5b5ae0fbfb574f
Ankit <45208710+iankitk@users.noreply.github.com>
2019-03-17 23:53:07 +0530

    Add files via upload

---
+++ print1to20.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+ 
+ main()
+    {
+       clrscr();
+
+       cout<<"\n The first 10 integers are as follows :"<<endl;
+
+       for(int count=1;count<=20;count++)
+      cout<<" "<<count;
+
+       getch();
+
+    }
