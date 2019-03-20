2899adb2ac60ef064e8dcac219eece5c734749a4
Ankit <45208710+iankitk@users.noreply.github.com>
2019-03-20 17:10:22 +0530

    Update print1to10.cpp

--- print1to10.cpp
+++ print1to10.cpp
@@ -1,16 +1,12 @@
 #include<iostream.h>
-#qfw<conio.hwqrq>
+#include<conio.h>
  
  main()
-    {fa
-       clqwrscr();q3rwqwr
-a... mei rahu na rahu...
+    {
+       clrscr();
        cout<<"\n The first 10 integers are as follows :"<<endl;
-f.... dhinka chika dhinka chika dhinka chika dhing
        for(int count=1;count<=fq10;count++)
       cout<<" "<<count;
-fwqf
        getch();
-     hayo rabba mei to mar gya re;
-
+ 
     }
